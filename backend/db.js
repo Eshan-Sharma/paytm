0c7a511cc40a44a07a6f023115de7a52b1b516f2
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-07 00:18:15 +0530

    debugged db

--- backend/db.js
+++ backend/db.js
@@ -1,38 +1,5 @@
 // backend/db.js
 const mongoose = require("mongoose");
-
-// Create a Schema for Users
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    minLength: 3,
-    maxLength: 30,
-  },
-  password: {
-    type: String,
-    required: true,
-    minLength: 6,
-  },
-  firstName: {
-    type: String,
-    required: true,
-    trim: true,
-    maxLength: 50,
-  },
-  lastName: {
-    type: String,
-    required: true,
-    trim: true,
-    maxLength: 50,
-  },
-});
-
-// Create a model from the schema
-
 const accountSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId, // Reference to User model
