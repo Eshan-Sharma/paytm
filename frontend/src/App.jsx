b47cc6f32dcb4039ff278e65684d551d280ce978
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-04-11 17:08:36 +0530

    added react routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <div>hey</div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
-
 export default App;
