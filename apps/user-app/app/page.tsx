5ea01534509cf382754277cf6e37e596bfa51d45
eshan-sharma <43044334+Eshan-Sharma@users.noreply.github.com>
2024-06-24 17:09:01 +0530

    cleanup

--- apps/user-app/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
-
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
